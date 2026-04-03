2ce6fdfb7a74aef32259453b680d2a8c7577b1e9
mongsil <mongsil@DESKTOP-EHE4671>
2026-04-03 12:28:02 +0900

    추가 변경

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -3,6 +3,6 @@ package sample;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaa");
-		System.out.println("Ãß°¡");
+		System.out.println("chang");
 	} 
 }
